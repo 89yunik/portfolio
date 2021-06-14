1fbe77e4736607dafa686179eabbda54bd528ac9
89yunik <89yunik@gmail.com>
2021-06-14 19:00:58 +0900

    project1 update

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,23 @@
+import project1_1 from '../project1_1.PNG'
+
 function Project() {
   return (
     <section className="section-project">
-      <div>
-        <h3>2021.4 ~ 2021.5</h3>
+      <div className="project-image-container">
+        <button className="prev-image">이전</button>
+        <img className="project-image" src={project1_1} alt="" />
+        <button className="next-image">다음</button>
+      </div>
+      <div className="project-desc-container">
+        <h3>2021.4.27 ~ 2021.6.12</h3>
         <h2>웹사이트 제작</h2>
-        <p>ReactJS, ExpressJS</p>
+        <p>Frontend : ReactJS<br/>Backend : Express</p>
         <ol>
           <li>
-            로그인ㆍ회원가입ㆍ비밀번호 찾기 기능
+            로그인ㆍ회원가입ㆍ회원정보수정ㆍ비밀번호 찾기 기능
           </li>
           <li>
-            목록ㆍ글쓰기ㆍ수정 기능
+            게시판ㆍ글쓰기ㆍ수정ㆍ삭제 기능
           </li>
         </ol>
       </div>
