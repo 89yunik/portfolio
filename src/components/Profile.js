bd444f0341194581ba56ac0c264644df7082f8b2
89yunik <89yunik@gmail.com>
2021-06-15 18:20:26 +0900

    change profile & project section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,7 @@
 function Profile() {
   return (
     <section className="section-profile">
-      {/* <img src="image-profile.jpg" className="image-profile"> */}
-      <div className="blank"></div>
-      <div>
+      <div className="container-profile">
         <dl className="name-link-profile">
           <dt className="name-profile">윤익</dt>
           <dd id="email-profile" className="link-profile" onClick={
@@ -11,17 +9,15 @@ function Profile() {
             }>이메일</dd>
           <dd id="github-profile" className="link-profile"><a href="https://github.com/89yunik">깃허브</a></dd>
         </dl>
-        <table>
-          <tbody>
-            <tr>
-              <td>2021.8</td>
-              <td>서울시립대학교 전자전기컴퓨터공학부 졸업(예정)</td>
-            </tr>
-            <tr>
-
-            </tr>
-          </tbody>
-        </table>
+        <ul className="desc-profile">
+          <li>신입 프론트엔드 개발자입니다</li>
+          <li>간결하고 실용적인 웹 애플리케이션 개발을 지향하고 있습니다</li>
+        </ul>
+        <h3>Skills</h3>
+        <ul className="desc-profile">
+          <li>Frontend : HTML5, CSS3, JavaScript - React</li>
+          <li>Backend : Node.js - Express</li>
+        </ul>
       </div>
     </section>
   )
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,33 @@
 import project1_1 from '../project1_1.PNG'
+// import prev from '../prev.svg'
 
 function Project() {
   return (
     <section className="section-project">
-      <div className="project-image-container">
-        <button className="prev-image">이전</button>
-        <img className="project-image" src={project1_1} alt="" />
-        <button className="next-image">다음</button>
-      </div>
-      <div className="project-desc-container">
-        <h3>2021.4.27 ~ 2021.6.12</h3>
-        <h2>웹사이트 제작</h2>
-        <p>Frontend : ReactJS<br/>Backend : Express</p>
-        <ol>
-          <li>
-            로그인ㆍ회원가입ㆍ회원정보수정ㆍ비밀번호 찾기 기능
-          </li>
-          <li>
-            게시판ㆍ글쓰기ㆍ수정ㆍ삭제 기능
-          </li>
-        </ol>
+      <div className="project-container">
+        <button className="prev">&lt;</button>
+        <div className="project-image-container">
+          <button className="prev">&lt;</button>
+          <img className="project-image" src={project1_1} alt="" />
+          <button className="next">&gt;</button>
+        </div>
+        <div className="project-desc-container">
+          <h3>2021.4.27 ~ 2021.6.12</h3>
+          <h2>웹사이트 제작</h2>
+          <ul className="project-desc-skills">
+            <li>Frontend : React</li>
+            <li>Backend : Express</li>
+          </ul>
+          <ol>
+            <li>
+              로그인ㆍ회원가입ㆍ회원정보수정ㆍ비밀번호 찾기 기능
+            </li>
+            <li>
+              게시판ㆍ글쓰기ㆍ수정ㆍ삭제 기능
+            </li>
+          </ol>
+        </div>
+        <button className="next">&gt;</button>
       </div>
     </section>
   )
